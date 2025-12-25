36cb650e8fe666e5a3665c98157ba3b3389d4ecd
shivamchauhan05 <chauhanshivam8836@gmail.com>
2025-12-25 10:18:45 +0530

    Fix CSS import case sensitivity issue

--- src/pages/explorepages.jsx
+++ src/pages/explorepages.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { useCart } from '../context/CartContext';
-import './Explore.css';
+import './explore.css';
 
 const Explore = () => {
   const { categoryId } = useParams();
